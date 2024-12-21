3748fe56bec32cf1af97bc1e9f7e4dc39f274631
Steusse <stephenusse@gmail.com>
2024-12-21 00:00:59 -0500

    Initial main class and added /build folder to gitignore

---
+++ src/main/java/su/Sudoku/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package su.Sudoku;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
